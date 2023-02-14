1b1c6eddb4367bea89c6879e0b343001a614e6eb
Samara Carvalho <samcarvalhos@hotmail.com>
2023-02-14 11:34:59 -0300

    Initial commit

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'ui'
+
+import '@settings/tailwind/globals.css'
+
+export default function Web() {
+  return (
+    <div>
+      <h1>Web</h1>
+      <Button />
+    </div>
+  )
+}
